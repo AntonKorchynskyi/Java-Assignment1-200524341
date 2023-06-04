47dda7071cc472b34d0b5f9a1053a50cc77b6c3b
Anton Korchynskyi <200524341@student.georgianc.on.ca>
2023-06-04 15:43:55 -0400

    More Basic Files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.javaassignment1200524341 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.javaassignment1200524341 to javafx.fxml;
     exports com.example.javaassignment1200524341;
 }
