a447e46ec4a78014ec56f35be86f1f68c688ea3c
Anton Korchynskyi <200524341@student.georgianc.on.ca>
2023-06-11 16:07:13 -0400

    Added database files to the resources folder

---
+++ src/main/resources/com/example/javaassignment1200524341/JavaAssignment_SQL.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `javaassign1`.`studentinfo` (
+  `StudentID` INT NOT NULL,
+  `Gender` VARCHAR(45) NULL,
+  `EthnicGroup` VARCHAR(45) NULL,
+  `LunchType` VARCHAR(45) NULL,
+  `TestPrep` VARCHAR(45) NULL,
+  `ParentMaritalStatus` VARCHAR(45) NULL,
+  `PracticeSport` VARCHAR(45) NULL,
+  `IsFirstChild` VARCHAR(45) NULL,
+  `NrSiblings` INT NULL,
+  `TransportMeans` VARCHAR(45) NULL,
+  `MathScore` INT NULL,
+  `ReadingScore` INT NULL,
+  `WritingScore` INT NULL,
+  PRIMARY KEY (`StudentID`));
+
+/*
+To add data to the table it is required to run the script at the top ->
+-> right click on the table name -> 'Table Data Import Wizard' -> choose my csv file (Expanded_data_with_more_features1.csv) 
+and just click 'next' a few times
+*/
+
+SELECT * FROM javaassign1.studentinfo;
